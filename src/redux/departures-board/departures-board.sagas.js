f1fc320c88555e457ccbdcd80401f6aee67af944
Valentin Dimitroff <26602880+ValentinnDimitroff@users.noreply.github.com>
2021-06-01 17:32:39 +0300

    Add filtering by current date for schedule

--- src/redux/departures-board/departures-board.sagas.js
+++ src/redux/departures-board/departures-board.sagas.js
@@ -4,11 +4,22 @@ import { apiRoutes, httpClient } from '../../api'
 
 const departuresBoardActions = slice.actions
 
+const getCurrentDate = () => {
+    const d = new Date()
+    const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
+    const mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d)
+    const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d)
+
+    const result = `${ye}-${mo}-${da}`
+
+    return result
+}
+
 function* fetchStopSchedule({ payload: stopId }) {
     const { data } = yield call(httpClient.request, apiRoutes.schedules, {
         filter: {
             stop: stopId,
-            date: new Date().toLocaleString(),
+            date: getCurrentDate(),
         },
         include: ['route'],
         fields: ['direction_id', 'departure_time'],
